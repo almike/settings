ec0f1e8eb415a8e300d599f739ccccc8794d46ec
mickael <mickael.dehez@gmail.com>
2016-09-15 21:31:04 +0800

    add -y to remove command

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # install on 14.04
 apt update
 apt install -y \
-	python-software-properties software-properties-common
-	git vim terminator silversearcher-ag htop \
-	gnome-shell ubuntu-gnome-desktop \
-	smplayer pavucontrol
+ python-software-properties software-properties-common \
+ git vim terminator silversearcher-ag htop \
+ gnome-shell ubuntu-gnome-desktop \
+ smplayer pavucontrol
 # fix for x265
-apt remove mplayer2
+apt remove -y mplayer2
 # tmux 2.0 instead of 1.8
 add-apt-repository -y ppa:pi-rho/dev
 apt update
