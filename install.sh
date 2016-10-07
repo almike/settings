61ce8088457cb94eb5e90f7b4ce2d84f79f54617
mickael <mickael.dehez@gmail.com>
2016-10-07 10:58:01 +0800

    latest version of pg is 9.6 now

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install on 14.04, requires sudo
 
-# postgresql 9.5 instead of 9.3
+# postgresql 9.x instead of 9.3
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
 
