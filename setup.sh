c07dfe481cbeeb3f647e612de391cb9855282246
almike <mickael.dehez@gmail.com>
2021-03-07 15:11:32 +0800

    remove os deps

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-for file in vimrc vim tmux.conf jshintrc; do
+for file in vimrc vim; do
 	rm -fr ~/.$file
 	ln -s $root/.$file ~/.$file
 done
@@ -9,14 +9,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone git@github.com:scrooloose/syntastic.git ~/.vim/bundle/syntastic
 git clone git@github.com:vim-airline/vim-airline.git ~/.vim/bundle/airline
 git clone git@github.com:rking/ag.vim.git ~/.vim/bundle/ag
-git clone git@github.com:derekwyatt/vim-scala.git ~/.vim/bundle/scala
 git clone git@github.com:ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp
 git config --global core.editor "vim"
 git config --global push.default simple
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 12
 npm config set loglevel silent
-
 echo "do: git config --global user.email/name"
